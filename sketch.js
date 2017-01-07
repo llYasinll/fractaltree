b9bcc75215699a07d33d9d830edde5cd342843fb
llYasinll <lloyasinll@gmail.com>
2017-01-07 16:10:05 -0700

    found a better way to deal with buttons

--- sketch.js
+++ sketch.js
@@ -6,25 +6,15 @@ var leaves = [];
 var canv;
 var btns = [];
 var clr = '#ffffff';
+
 function setup() {
     canv = createCanvas(600, 600);
     canv.parent('holding');
     var a = createVector(width / 2, height);
     var b = createVector(width / 2, height - len);
     branch = new Branch(a, b);
-    var Btn = document.getElementById('orng');
-    btns.push(Btn);
-    Btn = document.getElementById('rd');
-    btns.push(Btn);
-    Btn = document.getElementById('blu');
-    btns.push(Btn);
-    Btn = document.getElementById('yellow');
-    btns.push(Btn);
-    Btn = document.getElementById('grn');
-    btns.push(Btn);
     tree[0] = branch;
 }
-
 
 function draw() {
     background(51);
@@ -65,12 +55,6 @@ function keyPressed() {
 }
 
 function reply_click(click) {
-  for (var i = 0; i < btns.length; i++) {
-    if(btns[i].id === click){
-
-      clr = btns[i].value;
-
-    }
-  }
-
-    }
+    var Btn = select('#' + click);
+    clr = Btn.value();
+}
